59814ce1a40635be2ed30de7eadcfcfed492264f
Brian Jewkes <bdjewkes@gmail.com>
2018-01-01 21:27:19 -0400

    Level loader loads all 7 levels

---
+++ levels.js
@@ -0,0 +1,64 @@
+var levelsData = [
+    {
+        "mapKey": "map0",
+        "monsters": [
+            {"x": 350, "y": 250, "key": "spider", "scale": 3, "health": 3 },
+            {"x": 250, "y": 350, "key": "skeleton", "scale": 2, "health": 3 }
+        ],
+        "exits": { "top": 1 }
+    },
+    {
+        "mapKey": "map1",
+        "monsters": [
+            {"x": 350, "y": 250, "key": "spider", "scale": 3, "health": 3 },
+            {"x": 450, "y": 400, "key": "spider", "scale": 3, "health": 3 },
+            {"x": 250, "y": 350, "key": "skeleton", "scale": 2, "health": 5 }
+        ],
+        "exits": { 
+            "top": 2,
+            "bottom": 0
+        }
+    },
+    {
+        "mapKey": "map2",
+        "monsters": [],
+        "exits": { 
+            "top": 3,
+            "bottom": 1 
+        }
+    },
+    {
+        "mapKey": "map3",
+        "monsters": [],
+        "exits": { 
+            "top": 4,
+            "bottom": 2
+        }
+    },
+    {
+        "mapKey": "map4",
+        "monsters": [],
+
+        "exits": { 
+            "top": 5,
+            "bottom": 3
+        }
+    },
+    {
+        "mapKey": "map5",
+        "monsters": [],
+        "exits": { 
+            "top": 6,
+            "bottom": 4
+        }
+    },
+    {
+        "mapKey": "map6",
+        "monsters": [
+            {"x": 350, "y": 50, "key": "skeleton", "scale": 6, "health": 10 },
+        ],
+        "exits": { 
+            "bottom": 5
+        }
+    }
+]
